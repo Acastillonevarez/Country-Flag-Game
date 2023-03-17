f5a3da1a7a708b4319c75141936c18779d8a095d
Andrew <acastillonevarez@bsd220.org>
2023-03-17 09:25:41 -0500

    Initial Commit

---
+++ Country Flag Game/Country_Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_GameApp.swift
+//  Country Flag Game
+//
+//  Created by Andrew Castillo Nevarez on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
